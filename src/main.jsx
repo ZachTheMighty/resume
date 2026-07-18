ca6ca807293c603703b3818999bf0265ca40ab78
ZachTheMighty <promoted66@gmail.com>
2026-07-18 22:07:32 +0300

    add: basic css to body and root

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+document.body.className = "min-h-screen flex justify-center items-center";
+const root = document.getElementById("root");
+root.className = "flex flex-col items-center";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
