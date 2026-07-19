38637e8564515939ab74d708d5aab29f4e7e5032
ZachTheMighty <promoted66@gmail.com>
2026-07-19 17:44:38 +0300

    change: used tailwind built in class for max width value instead of a custom one

--- src/components/sections/section.jsx
+++ src/components/sections/section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ sectionName, children }) {
   return (
-    <div className="mb-15 max-w-[700px]">
+    <div className="mb-15 max-w-2xl">
       <h1 className="mb-4 text-2xl font-light">{sectionName}</h1>
       <div className="flex flex-wrap justify-start items-center gap-4">
         {children}
