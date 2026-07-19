c1daeadcdf082c34dbd891108f6a4d82e94964b4
ZachTheMighty <promoted66@gmail.com>
2026-07-19 15:04:40 +0300

    add: header to general section

--- src/components/general.jsx
+++ src/components/general.jsx
@@ -2,10 +2,13 @@ import Input from "./input.jsx";
 
 export default function () {
   return (
-    <div className="flex justify-center items-center gap-4">
-      <Input type="text" name="Name" />
-      <Input type="email" name="Email" />
-      <Input type="tel" name="Tel" />
+    <div>
+      <h1 className="mb-6 text-2xl font-light">General information</h1>
+      <div className="flex justify-center items-center gap-4">
+        <Input type="text" name="Name" />
+        <Input type="email" name="Email" />
+        <Input type="tel" name="Tel" />
+      </div>
     </div>
   );
 }
