9152e0770478b9f87ca0f299c89d14b8f7f00dfe
ZachTheMighty <promoted66@gmail.com>
2026-07-19 18:28:34 +0300

    add: basic idea to how I want to implement this

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 import General from "./components/sections/general.jsx";
 import Education from "./components/sections/education.jsx";
 import Practical from "./components/sections/practical.jsx";
 
 export default function App() {
+  const [resume, setResume] = useState(false);
+
+  const root = document.querySelector("#root");
+
+  function handleGenerate() {
+    root.classList.toggle("bg-teal-400");
+    setResume(true);
+  }
+
+  function handleEdit() {
+    root.classList.toggle("bg-teal-400");
+    setResume(false);
+  }
+
   return (
     <>
       <General />
       <Education />
       <Practical />
-      <button>Generate resume</button>
+      {!resume ? <button onClick={handleGenerate}>Generate resume</button> :
+        <button onClick={handleEdit}>Edit resume</button>
+      }
     </>
   );
 }
