08d7d0c46fd5a859a8ab7e11129a99fa01ee0817
ZachTheMighty <promoted66@gmail.com>
2026-07-19 15:30:06 +0300

    change: decreased margin in section headers

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,7 +1,7 @@
 export default function Section({ sectionName, children }) {
   return (
     <div className="mb-15">
-      <h1 className="mb-6 text-2xl font-light">{sectionName}</h1>
+      <h1 className="mb-4 text-2xl font-light">{sectionName}</h1>
       <div className="flex justify-center items-center gap-4">{children}</div>
     </div>
   );
