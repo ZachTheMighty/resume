19e2806287237594571fbf6646c29478a1bdceb7
ZachTheMighty <promoted66@gmail.com>
2026-07-19 16:42:51 +0300

    add: max width to sections

--- src/components/sections/section.jsx
+++ src/components/sections/section.jsx
@@ -1,8 +1,8 @@
 export default function Section({ sectionName, children }) {
   return (
-    <div className="mb-15 ">
+    <div className="mb-15 max-w-[700px]">
       <h1 className="mb-4 text-2xl font-light">{sectionName}</h1>
-      <div className="flex flex-wrap justify-center items-center gap-4">
+      <div className="flex flex-wrap justify-start items-center gap-4">
         {children}
       </div>
     </div>
