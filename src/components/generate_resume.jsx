9a9b40a3c80a5655fd203f951ccc508b82b787b4
ZachTheMighty <promoted66@gmail.com>
2026-07-19 19:40:53 +0300

    chore: formatted file

--- src/components/generate_resume.jsx
+++ src/components/generate_resume.jsx
@@ -2,13 +2,13 @@ import General from "./sections/general.jsx";
 import Education from "./sections/education.jsx";
 import Practical from "./sections/practical.jsx";
 
-export default function GenerateResume({onGenerate}) {
-    return (
-        <>
-        <General />
-        <Education />
-        <Practical />
-<button onClick={onGenerate}>Generate resume</button>
-        </>
-    );
+export default function GenerateResume({ onGenerate }) {
+  return (
+    <>
+      <General />
+      <Education />
+      <Practical />
+      <button onClick={onGenerate}>Generate resume</button>
+    </>
+  );
 }
