b47ed3f6ca893e4875045a0914ac06bab3e9dc9e
ZachTheMighty <promoted66@gmail.com>
2026-07-19 15:29:35 +0300

    add: margin to sections to seperate them

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ sectionName, children }) {
   return (
-    <div>
+    <div className="mb-15">
       <h1 className="mb-6 text-2xl font-light">{sectionName}</h1>
       <div className="flex justify-center items-center gap-4">{children}</div>
     </div>
