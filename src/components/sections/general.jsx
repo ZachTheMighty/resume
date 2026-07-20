3d523924dce27c5130787cfc27b26f8721972854
ZachTheMighty <promoted66@gmail.com>
2026-07-20 20:38:59 +0300

    add: styles to upload photo button

--- src/components/sections/general.jsx
+++ src/components/sections/general.jsx
@@ -44,7 +44,11 @@ export default function General({ values, setValues }) {
       <div className="mt-8">
         <label htmlFor="file">
           <div className="mb-2">Upload photo</div>
-          <div className="flex justify-center bg-gray-800 rounded-md">
+
+          <div
+            tabIndex="0"
+            className="flex justify-center bg-indigo-500 rounded-md py-2 text-xl font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2"
+          >
             <img src={plus} alt="plus sign" className="w-10" />
           </div>
         </label>
