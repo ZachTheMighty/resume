abd53a16f4ddfe97b33a22e00ef5340d84c5e008
ZachTheMighty <promoted66@gmail.com>
2026-07-20 20:19:38 +0300

    remove: required attribute from file input field
    Since the input element's display is set to none and it's also required, the browser can't display the message "Please fill out this field" and so it logs it as an error instead. Fixed it by removed the required attribute altogther

--- src/components/sections/general.jsx
+++ src/components/sections/general.jsx
@@ -50,7 +50,6 @@ export default function General({ values, setValues }) {
         </label>
 
         <input
-          required
           type="file"
           id="file"
           accept="image/*"
