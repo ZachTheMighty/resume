ffefa69241cd858bc1e81b242870afe29372a763
ZachTheMighty <promoted66@gmail.com>
2026-07-21 16:02:29 +0300

    add: static button for adding education experience

--- src/components/sections/education.jsx
+++ src/components/sections/education.jsx
@@ -1,6 +1,8 @@
 import Input from "../inputs/input.jsx";
 import Section from "./section.jsx";
 
+import plus from "../../assets/plus.svg";
+
 export default function Education({ values, setValues }) {
   return (
     <Section sectionName={"Educational experience"}>
@@ -19,6 +21,13 @@ export default function Education({ values, setValues }) {
         />
         <Input type="date" name="From" values={values} setValues={setValues} />
         <Input type="date" name="To" values={values} setValues={setValues} />
+        <div
+          tabIndex="0"
+          className="col-span-2 flex justify-center items-center bg-indigo-500 rounded-md py-2 text-xl font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2"
+        >
+          <div>Add experience</div>
+          <img src={plus} alt="plus sign" className="w-8" />
+        </div>
       </div>
     </Section>
   );
