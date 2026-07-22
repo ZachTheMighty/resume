7906724e4459093053fd6ca86a12de08b3317b1a
ZachTheMighty <promoted66@gmail.com>
2026-07-22 18:24:13 +0300

    fix: photo not disappearing upon resetting the form

--- src/components/sections/general.jsx
+++ src/components/sections/general.jsx
@@ -65,7 +65,7 @@ export default function General({ general, setGeneral }) {
         />
 
         <div className="mt-4 flex justify-center">
-          {file && <img src={URL.createObjectURL(file)} className="w-md" />}
+          {file && <img src={general.photo} className="w-md" />}
         </div>
       </div>
     </Section>
