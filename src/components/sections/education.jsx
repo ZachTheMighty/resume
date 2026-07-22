702bd3a68c6d9850f62869cffb4f140da20d90e6
ZachTheMighty <promoted66@gmail.com>
2026-07-22 15:42:41 +0300

    fix: education experience could still be added even if the form is not completely filled out

--- src/components/sections/education.jsx
+++ src/components/sections/education.jsx
@@ -8,7 +8,10 @@ export default function Education({ education, setEducation, edus, setEdus }) {
     <Section sectionName={"Educational experience"}>
       <form
         className="grid grid-cols-2 gap-6"
-        onSubmit={(event) => event.preventDefault()}
+        onSubmit={(event) => {
+          event.preventDefault();
+          setEdus([...edus, education]);
+        }}
       >
         <Input
           type="text"
@@ -34,10 +37,7 @@ export default function Education({ education, setEducation, edus, setEdus }) {
           values={education}
           setValues={setEducation}
         />
-        <button
-          className="col-span-2 flex justify-center items-center bg-indigo-500 rounded-md py-4 text-xl font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2"
-          onClick={() => setEdus([...edus, education])}
-        >
+        <button className="col-span-2 flex justify-center items-center bg-indigo-500 rounded-md py-4 text-xl font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2">
           <div>Add experience</div>
           <img src={plus} alt="plus sign" className="w-8" />
         </button>
