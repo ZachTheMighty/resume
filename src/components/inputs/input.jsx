57e573badb09838fc35ece828270c1a3ec737476
ZachTheMighty <promoted66@gmail.com>
2026-07-22 19:16:09 +0300

    feat: added success indicator upon adding educational experience

--- src/components/inputs/input.jsx
+++ src/components/inputs/input.jsx
@@ -1,6 +1,6 @@
 import camelCase from "./camel_case.js";
 
-export default function Input({ type, name, values, setValues }) {
+export default function Input({ type, name, values, setValues, setAdded }) {
   return (
     <div className="flex flex-col gap-2 flex-1">
       <label className="text-sm">{name}</label>
@@ -9,12 +9,13 @@ export default function Input({ type, name, values, setValues }) {
         type={type}
         value={values[camelCase(name)]}
         className="outline outline-white/15 text-focus:outline-offset-2 px-3 py-1.5 focus:outline-2 focus:outline-indigo-500 rounded-md bg-white/5"
-        onChange={(event) =>
+        onChange={(event) => {
           setValues({
             ...values,
             [camelCase(name)]: event.target.value,
-          })
-        }
+          });
+          setAdded && setAdded(false);
+        }}
       />
     </div>
   );
--- src/components/sections/education.jsx
+++ src/components/sections/education.jsx
@@ -2,8 +2,11 @@ import Input from "../inputs/input.jsx";
 import Section from "./section.jsx";
 
 import plus from "../../assets/plus.svg";
+import { useState } from "react";
 
 export default function Education({ education, setEducation, edus, setEdus }) {
+  const [added, setAdded] = useState(false);
+
   function resetForm() {
     setEducation({
       schoolName: "",
@@ -21,6 +24,7 @@ export default function Education({ education, setEducation, edus, setEdus }) {
           event.preventDefault();
           setEdus([...edus, education]);
           resetForm();
+          setAdded(true);
         }}
       >
         <Input
@@ -28,30 +32,40 @@ export default function Education({ education, setEducation, edus, setEdus }) {
           name="School name"
           values={education}
           setValues={setEducation}
+          setAdded={setAdded}
         />
         <Input
           type="text"
           name="Title of study"
           values={education}
           setValues={setEducation}
+          setAdded={setAdded}
         />
         <Input
           type="date"
           name="From"
           values={education}
           setValues={setEducation}
+          setAdded={setAdded}
         />
         <Input
           type="date"
           name="To"
           values={education}
           setValues={setEducation}
+          setAdded={setAdded}
         />
         <button className="col-span-2 flex justify-center items-center bg-indigo-500 rounded-md py-4 text-xl font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2">
           <div>Add experience</div>
           <img src={plus} alt="plus sign" className="w-8" />
         </button>
       </form>
+
+      {added && (
+        <div className="mt-4 text-xl font-semibold text-green-600">
+          Experience has been added!
+        </div>
+      )}
     </Section>
   );
 }
