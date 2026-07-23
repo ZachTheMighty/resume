49a7b1aa3667880eef137038ec6cb057e75d869a
ZachTheMighty <promoted66@gmail.com>
2026-07-23 19:56:00 +0300

    remove: useless variable

--- src/App.jsx
+++ src/App.jsx
@@ -123,8 +123,6 @@ export default function App() {
   const [skill, setSkill] = useState(skillsInit);
   const [skills, setSkills] = useState([]);
 
-  const root = document.querySelector("#root");
-
   function handleGenerate() {
     setResume(true);
   }
