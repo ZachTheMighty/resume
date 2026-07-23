120dc0494f9733476a827d0d48fd17c058543ba4
ZachTheMighty <promoted66@gmail.com>
2026-07-23 20:19:30 +0300

    render: full name

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,12 +1,16 @@
 export default function Resume({ general }) {
   return (
     <div className="flex-1 flex min-h-screen">
-      <div className="flex-1 flex flex-col items-center mt-24 bg-gray/10 min-h-full">
+      <div className="flex-1 flex flex-col gap-8 items-center mt-24 bg-gray/10 min-h-full">
         <img
           src={general.photo}
           alt="profile picture"
           className="w-64 h-64 rounded-full object-cover"
         />
+
+        <h1 className="text-2xl font-extrabold">
+          {general.first.value} {general.last.value}
+        </h1>
       </div>
       <div className="flex-2 bg-[#172131] min-h-full"></div>
     </div>
