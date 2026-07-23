67cafffbf0dbb158522022b957829eb39f369bc2
ZachTheMighty <promoted66@gmail.com>
2026-07-23 20:41:04 +0300

    render: current school

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,4 +1,6 @@
-export default function Resume({ general }) {
+import school from "../assets/town-hall.svg";
+
+export default function Resume({ general, edus }) {
   return (
     <div className="flex-1 flex min-h-screen">
       <div className="flex-1 flex flex-col gap-8 items-center mt-24 bg-gray/10 min-h-full">
@@ -8,9 +10,17 @@ export default function Resume({ general }) {
           className="w-64 h-64 rounded-full object-cover"
         />
 
-        <h1 className="text-2xl font-extrabold">
+        <div className="text-2xl font-extrabold">
           {general.first.value} {general.last.value}
-        </h1>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <img src={school} alt="school icon" className="w-8" />
+          <div className="flex flex-wrap">
+            <div className="font-semibold mr-2">School:</div>
+            <div>{edus.at(-1).school.value}</div>
+          </div>
+        </div>
       </div>
       <div className="flex-2 bg-[#172131] min-h-full"></div>
     </div>
