f15df896cf91d63e5e8ccd985874495bbb1cb528
ZachTheMighty <promoted66@gmail.com>
2026-07-23 20:10:30 +0300

    add: rendered photo and gave it styles

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,7 +1,13 @@
-export default function Resume({}) {
+export default function Resume({ general }) {
   return (
     <div className="flex-1 flex min-h-screen">
-      <div className="flex-1 bg-gray/10 min-h-full"></div>
+      <div className="flex-1 flex flex-cols justify-center mt-24 bg-gray/10 min-h-full">
+        <img
+          src={general.photo}
+          alt="profile picture"
+          className="w-64 h-64 rounded-full object-cover"
+        />
+      </div>
       <div className="flex-2 bg-[#172131] min-h-full"></div>
     </div>
   );
