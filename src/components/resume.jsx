42b8d3435a367389132a67e6364dcc5e1efdf2f5
ZachTheMighty <promoted66@gmail.com>
2026-07-23 19:54:31 +0300

    add: styles to generated resume

---
+++ src/components/resume.jsx
@@ -0,0 +1,8 @@
+export default function Resume({}) {
+  return (
+    <div className="flex-1 flex min-h-screen">
+      <div className="flex-1 bg-gray/10 min-h-full"></div>
+      <div className="flex-2 bg-[#172131] min-h-full"></div>
+    </div>
+  );
+}
