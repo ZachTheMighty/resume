04e45b063ca300c42cb29ad9e05bd6072914a898
ZachTheMighty <promoted66@gmail.com>
2026-07-24 20:23:53 +0300

    add: module to format date

---
+++ src/utils/date_formatter.jsx
@@ -0,0 +1,34 @@
+export default function formatter(date) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const arr = date.split("-");
+  let sup = "";
+
+  if (+arr[2] === 1) sup = "st";
+  else if (+arr[2] === 2) sup = "nd";
+  else if (+arr[2] === 3) sup = "rd";
+  else sup = "th";
+
+  return (
+    <div>
+      {months[+arr[1] - 1]} {+arr[2]}
+      <sup>{sup}, </sup>
+      {arr[0]}
+    </div>
+  );
+}
+
+console.log(formatter("2002-09-28"));
