4d449ea654dc860dced3e62b4e70c4dfcd70953b
ZachTheMighty <promoted66@gmail.com>
2026-07-24 17:09:08 +0300

    fix: icons would shrink down if they had to
    fixed by adding min height

--- src/components/basic_info.jsx
+++ src/components/basic_info.jsx
@@ -2,7 +2,7 @@ export default function BasicInfo({ img, alt, header, content }) {
   return (
     <div className="w-full md:w-[60%] flex items-center gap-2">
       <div className="rounded-full bg-indigo-500 p-1 sm:p-2 ml-4">
-        <img src={img} alt={alt} className="min-w-4 sm:w-6 md:w-8" />
+        <img src={img} alt={alt} className="min-w-4 sm:min-w-6 md:min-w-8" />
       </div>
 
       <div className="flex flex-wrap">
