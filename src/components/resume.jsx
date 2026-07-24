4f59635a110cc0a1126e1bfc2fe06c45052d2851
ZachTheMighty <promoted66@gmail.com>
2026-07-24 14:06:19 +0300

    add: default photo in case one wasn't provided

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,12 +1,14 @@
 import BasicInfo from "./basic_info.jsx";
+
 import school from "../assets/town-hall.svg";
+import defaultPhoto from "../assets/default_photo.jpg";
 
 export default function Resume({ general, edus }) {
   return (
     <div className="flex-1 flex min-h-screen">
       <div className="flex-1 flex flex-col gap-8 items-center mt-24 bg-gray/10 min-h-full">
         <img
-          src={general.photo}
+          src={general.photo ? general.photo : defaultPhoto}
           alt="profile picture"
           className="w-64 h-64 rounded-full object-cover"
         />
