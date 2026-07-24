b229a54b39bf0f46b872abd2105e9d0995df8162
ZachTheMighty <promoted66@gmail.com>
2026-07-24 17:19:25 +0300

    change: set width of basic components to full

--- src/components/basic_info.jsx
+++ src/components/basic_info.jsx
@@ -1,6 +1,6 @@
 export default function BasicInfo({ img, alt, header, content }) {
   return (
-    <div className="w-full md:w-[60%] flex items-center gap-2">
+    <div className="w-full flex items-center gap-2">
       <div className="rounded-full bg-indigo-500 p-1 sm:p-2 ml-4">
         <img src={img} alt={alt} className="min-w-4 sm:min-w-6 md:min-w-8" />
       </div>
