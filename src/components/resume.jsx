1075ae80fdc7c8be51b71698a5b0cdcd2ccc1afc
ZachTheMighty <promoted66@gmail.com>
2026-07-25 20:14:45 +0300

    feat: add functionality to print resume button

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -16,6 +16,9 @@ import medal from "../assets/medal.svg";
 import tools from "../assets/tools.svg";
 import briefcase from "../assets/briefcase.svg";
 
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+
 export default function Resume({
   general,
   edus,
@@ -23,9 +26,16 @@ export default function Resume({
   skills,
   practicals,
 }) {
+  const componentRef = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    contentRef: componentRef,
+    documentTitle: "Receipt_Data",
+  });
+
   return (
-    <div className="flex-1 flex min-h-screen">
-      <div className="flex-1 flex flex-col gap-8 items-center pt-16 bg-gray/10 min-h-full">
+    <div ref={componentRef} className="flex-1 flex min-h-screen">
+      <div className="flex-1 flex flex-col gap-8 items-center pt-16 bg-gray/10 print:bg-gray/10 min-h-full">
         <img
           src={general.photo || defaultPhoto}
           alt="profile picture"
@@ -105,8 +115,11 @@ export default function Resume({
           ))}
         />
 
-        <div className="self-end flex sm:block sm:mb-10 md:mb-20 text-xs sm:text-xl md:text-2xl">
-          <button className="flex-1 bg-indigo-500 rounded-md px-3 py-2 font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2">
+        <div className="self-end flex sm:block sm:mb-10 md:mb-20 text-xs sm:text-xl md:text-2xl print:hidden">
+          <button
+            onClick={handlePrint}
+            className="flex-1 bg-indigo-500 rounded-md px-3 py-2 font-semibold hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2"
+          >
             Print resume
           </button>
         </div>
