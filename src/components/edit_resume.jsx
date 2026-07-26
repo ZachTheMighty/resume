a356fdeb5d98282f192b665999d1b6a930f2133d
ZachTheMighty <promoted66@gmail.com>
2026-07-26 19:27:50 +0300

    refactor: edit resume layout to table instead of flex bullshit add: items' content in table

--- src/components/edit_resume.jsx
+++ src/components/edit_resume.jsx
@@ -1,16 +1,36 @@
 export default function EditResume({ items }) {
   return (
-    <div className="mt-16 outline outline-white/10">
-      <div className="flex justify-evenly text-xs sm:text-base md:text-xl bg-white/5 py-4">
-        {Object.entries(items[0]).map(
-          ([key, value], index) =>
-            key !== "sectionName" && (
-              <div key={index} className="">
-                {value.label}
-              </div>
-            ),
-        )}
-      </div>
-    </div>
+    <table className="mt-16 mx-auto">
+      <thead>
+        <tr className="text-xs sm:text-base md:text-xl bg-white/5">
+          {Object.entries(items[0]).map(
+            ([key, value], index) =>
+              key !== "sectionName" && (
+                <th
+                  key={index}
+                  className="p-4 border border-white/10 text-start"
+                >
+                  {value.label}
+                </th>
+              ),
+          )}
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item) => (
+          <tr className="text-xs sm:text-base md:text-xl even:bg-white/5 odd:bg-white/10">
+            {Object.entries(item).map(
+              ([key, item]) =>
+                key !== "sectionName" && (
+                  <td key={key} className="p-4 border border-white/10">
+                    {item.value}
+                  </td>
+                ),
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
