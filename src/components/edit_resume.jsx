7da8d5f171a14ca0bff7b1e4f3223461206bff09
ZachTheMighty <promoted66@gmail.com>
2026-07-28 19:45:44 +0300

    feat: add responsiviness to items table

--- src/components/edit_resume.jsx
+++ src/components/edit_resume.jsx
@@ -4,13 +4,13 @@ export default function EditResume({ items, setItems }) {
   return (
     <table className="mt-16 mx-auto">
       <thead>
-        <tr className="text-xs sm:text-base md:text-xl bg-white/5">
+        <tr className="block sm:table-row text-xs sm:text-base md:text-xl bg-white/5">
           {Object.entries(items[0]).map(
             ([key, value], index) =>
               key !== "sectionName" && (
                 <th
                   key={index}
-                  className="p-2 sm:p-4 border border-white/10 text-start"
+                  className="block sm:table-cell p-2 sm:p-4 border border-white/10 text-start"
                 >
                   {value.label}
                 </th>
@@ -23,12 +23,15 @@ export default function EditResume({ items, setItems }) {
         {items.map((item, rowIndex) => (
           <tr
             key={rowIndex}
-            className="text-xs sm:text-base md:text-xl even:bg-white/5 odd:bg-white/10 relative"
+            className="block sm:table-row text-xs sm:text-base md:text-xl even:bg-white/5 odd:bg-white/10 relative"
           >
             {Object.entries(item).map(
               ([key, item]) =>
                 key !== "sectionName" && (
-                  <td key={key} className="p-2 border border-white/10">
+                  <td
+                    key={key}
+                    className="block sm:table-cell p-2 border border-white/10"
+                  >
                     <input
                       className="w-full text-focus:outline-offset-2 px-3 py-1.5 focus:outline-2 focus:outline-indigo-500 rounded-md"
                       type={item.type}
